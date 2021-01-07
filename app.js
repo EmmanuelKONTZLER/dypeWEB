f05e0785384825c885cad783ee50eb0e8e5433ab
Emmanuel KONTZLER <kontzleremmanuel@yahoo.fr>
2021-01-07 19:16:54 +0100

    config pour heroku

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'reactapp/build')));
 
 app.get('/*', (req,res)=> {
-  res.sendFile(path.join(__dirname,'/reactapp/build'));
+  res.sendFile(path.join(__dirname,'/reactapp/build/App.js', 'App.js'));
 });
 
 app.use('/', indexRouter);
