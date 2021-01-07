66bfa1d39066ce989cc609bb055b300bc701a36e
Emmanuel KONTZLER <kontzleremmanuel@yahoo.fr>
2021-01-07 14:32:05 +0100

    fonction ajout de photos depuis galerie ordi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* --- Lien avec la connexion BDD --- */
 require('./connexion/connexion')
 
+var fileUpload = require('express-fileupload');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -10,12 +11,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(fileUpload());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -32,12 +32,6 @@ cloudinary.config({
 
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
 /* --- sign-up --- */
 router.post('/sign-up', async function(req, res, next) {
 
@@ -57,28 +51,25 @@ router.post('/sign-up', async function(req, res, next) {
   if (await userModel.findOne({email:req.body.email1})) {
     error.push('Utilisateur déja inscrit')
   }
-  console.log('B00', error)
-
+ 
   if (error[0] != undefined) {
     res.json({result:false, error});
   } else {
-    console.log('B02 pas d\'erreur')
-      var salt = uid2(32)
+    
+    var salt = uid2(32)
 
-      var user = new userModel({		
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email1,
-        salt: salt,
-        password: SHA256(req.body.password1 + salt).toString(encBase64),
-        token: uid2(32),
+    var user = new userModel({		
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email1,
+      salt: salt,
+      password: SHA256(req.body.password1 + salt).toString(encBase64),
+      token: uid2(32),      
+    });		
         
-      });		
-          
-      user = await user.save();		
-      console.log('b01', user)
+    user = await user.save();		
 
-      res.json({result:true,user});
+    res.json({result:true,user});
    }
 });
 
@@ -96,14 +87,9 @@ console.log('arrivée sign-up')
   }
   console.log('b10', )
   
-console.log('B04', error)
-console.log('B08', req.body.email2)
-console.log('B08s', error[0])
-
   if (error[0] == undefined) {
     var user = await userModel.findOne({email:req.body.email2})
   }
-  console.log('B09', user)
 
   if(user == null){
     error.push('E-mail incorrect')
@@ -111,8 +97,7 @@ console.log('B08s', error[0])
   if (user.password != SHA256(req.body.password2 + user.salt).toString(encBase64)) {
     error.push('Mot de passe incorrect');
   }
-  
-  console.log('B11', error)
+
 
   if (error[0] != undefined) {
     res.json({result:false, error});
@@ -127,33 +112,28 @@ console.log('B08s', error[0])
 /* --- afficher les communautés --- */
 router.get('/get-community', async function(req, res, next) {
   var community00 = await communauteModel.find({})
-  console.log('B13', community00)
   var community = [];
   for (var i=0 ; i<community00.length ; i++) {
     var members = await adhesionModel.find({idCommunaute:community00[i]._id});
     community.push({communityId:community00[i]._id, name:community00[i].name, urlLogo:community00[i].urlLogo, colorLogo:community00[i].colorLogo, count:members.length})
   };
-  console.log('B14', community)
   res.json({community});
 });
 
 
 /* --- adhérer au(x) commnauté(s) --- */
-
 router.post('/adhesion', async function(req, res, next) {
-console.log('BE Adhesion', req.body)
-var user = await userModel.findOne({token:req.body.token})
-date = new Date ()
-      var adhesion = new adhesionModel({		
-        idUser: user._id,
-        idCommunaute: req.body.communityId,
-        date: date,
-        });		
-          
-      adhesion = await adhesion.save();		
-      
-
-      res.json({adhesion});
+  var user = await userModel.findOne({token:req.body.token})
+  date = new Date ()
+  var adhesion = new adhesionModel({		
+    idUser: user._id,
+    idCommunaute: req.body.communityId,
+    date: date,
+ });		
+            
+  adhesion = await adhesion.save();		
+  
+  res.json({adhesion});
    
 });
 
@@ -162,99 +142,124 @@ date = new Date ()
 router.get('/get-profil', async function(req, res, next) {
   var user = await userModel.findOne({token:req.query.token})
   var myadhesion = await adhesionModel.find({idUser:user._id})
-  console.log('B13', user, myadhesion)
 
   var myCommunity = []
   for (var i=0 ; i<myadhesion.length ; i++) {
     var community = await communauteModel.findOne({_id:myadhesion[i].idCommunaute});
-    console.log('b14', community)
     var members = await adhesionModel.find({idCommunaute:community._id});
     myCommunity.push({communityId:community._id, name:community.name, urlLogo:community.urlLogo, colorLogo:community.colorLogo, count:members.length})
   }
   
-
   res.json({user, myCommunity});
 });
 
+
 /* --- modifier le profil --- */
 router.post('/change-profil', async function(req, res, next) {
-  console.log('BE changeP', req.body)
 
   var user = await userModel.findOne({token:req.body.token})
 
   if (req.body.key == 'first') {
-      user.firstName = req.body.firstName;
-      user = await user.save();
+    user.firstName = req.body.firstName;
+    user = await user.save();
   }
   if (req.body.key == 'last') {
-      user.lastName = req.body.lastName;
-      user = await user.save();
-  }
-            
+    user.lastName = req.body.lastName;
+    user = await user.save();
+  }      
 	
   res.json({user});
 });
 
-/* --- test photo --- */
+/* --- Modifier la photo d'avatar --- */
 router.post('/avatar', async function(req, res, next) {
-  console.log('BE avatar', req.body.avatar)
-  console.log('BE2 avatar', req.body)
+  
+  if (req.files) {
+    
+      console.log('BE avatar 1234', req.files)
+      var imagePath = './tmp/picture'+ uniqid()+'.jpg';
+      var resultCopy = await req.files.picture.mv(imagePath);
+      try {var resultCloudinary = await cloudinary.uploader.upload(imagePath)}
+      catch(error) { console.log('error', error); var erreur = error}
+      console.log('be4', resultCloudinary.secure_url)
+      
+      if(!erreur) {
+      res.json({result: true, url:resultCloudinary.secure_url} );
+      } else {
+      res.json({result: false, message: erreur} );
+      }
+      fs.unlinkSync(imagePath);
 
-  // var imagePath = './tmp/avatar'+ uniqid()+'.jpg';
-  // var resultCopy = await req.body.avatar.mv(imagePath);
-  var resultCloudinary = await cloudinary.uploader.upload(req.body.avatar);
-  console.log('be4', resultCloudinary.secure_url)
-  //console.log('be5', resultCopy)
+  } else { 
 
-  // if(!resultCopy) {
-  // res.json({result: true, avatar: resultCloudinary.url} );
-  // } else {
-  // res.json({result: false, message: 'error'} );
-  // }
-  // fs.unlinkSync(imagePath);
-  // }
-	
-  res.json({avatar: resultCloudinary.url});
+      if(req.body.url) {
+        var me = await userModel.findOne({token:req.body.token});
+        me.urlAvatar = req.body.url;
+        me = await me.save();
+        res.json({result: true, url:me.urlAvatar} );
+      }
+  }
 });
+
 
 /* --- récupérer les bons plans--- */
 router.get('/get-bonplan', async function(req, res, next) {
-  console.log('b30 arrivee BE bonplan', req.query)
 
   if (req.query.key == 1) {
-    var bonplan00 = await bonPlanModel.find()
+    if (req.query.categorie == '') {
+      var bonplan00 = await bonPlanModel.find()
+    }else {
+      var bonplan00 = await bonPlanModel.find({categorie:req.query.categorie})
+    }
+
     var user = await userModel.findOne({token:req.query.token})
+    var userCommunity = await adhesionModel.find({idUser:user._id})
     var bonplan = []
     for (i=0 ; i<bonplan00.length ; i++) {
       var posteur = await userModel.findOne({_id:bonplan00[i].idUserPosteur})
-      if (user.idBonPlan.indexOf(bonplan00[i]._id) == -1) {
-       bonplan.push({_id:bonplan00[i]._id, urlphoto:bonplan00[i].urlphoto, token:posteur.token, titre:bonplan00[i].titre, categorie:bonplan00[i].categorie , description:bonplan00[i].description, isliked:false, posteurFirstName:posteur.firstName, posteurAvatar:posteur.urlavatar})
-      } else { bonplan.push({_id:bonplan00[i]._id, urlphoto:bonplan00[i].urlphoto, token:posteur.token, titre:bonplan00[i].titre, categorie:bonplan00[i].categorie , description:bonplan00[i].description, isliked:true, posteurFirstName:posteur.firstName, posteurAvatar:posteur.urlavatar})
+        // Pour chaque bonPlan, controle de l'appartenance à une même communauté entre l'user et le posteur
+      var posteurCommunity = await adhesionModel.find({idUser:posteur._id})
+      var check = false
+        // il est apparu que la fonction qui comparait les communautés d'utilisateur ne reconnaissait pas les formats issus de la requete BDD
+        // le fait de passer par le format chaine de caractère JSON haronise et rend lisible les données
+      userCommunity = JSON.stringify(userCommunity)
+      userCommunity = JSON.parse(userCommunity)
+      posteurCommunity = JSON.stringify(posteurCommunity)
+      posteurCommunity = JSON.parse(posteurCommunity)
+        for (var j=0 ; j<userCommunity.length ; j++) {
+          for (var k=0 ; k<posteurCommunity.length ; k++) {
+            console.log ('check', userCommunity[j].idCommunaute , posteurCommunity[k].idCommunaute)
+            if (userCommunity[j].idCommunaute == posteurCommunity[k].idCommunaute ) {
+              check = true
+            }
+          }
+        }
+        // si le bonplan est liké par l'user, le bonplan apparait avec ine propriété isliked à true pour afficher le coeur rouge
+        if(check == true) {
+      if (user.idBonPlan.indexOf(bonplan00[i]._id)) {
+       bonplan.push({_id:bonplan00[i]._id, urlphoto:bonplan00[i].urlphoto, token:posteur.token, titre:bonplan00[i].titre, categorie:bonplan00[i].categorie , description:bonplan00[i].description, isliked:false, posteurFirstName:posteur.firstName, posteurAvatar:posteur.urlAvatar})
+      } else { bonplan.push({_id:bonplan00[i]._id, urlphoto:bonplan00[i].urlphoto, token:posteur.token, titre:bonplan00[i].titre, categorie:bonplan00[i].categorie , description:bonplan00[i].description, isliked:true, posteurFirstName:posteur.firstName, posteurAvatar:posteur.urlAvatar})
       }
     }
+  }
   } else {
   
     if (req.query.key == 2) {
-      console.log('get favorite', req.query)
       var user = await userModel.findOne({token:req.query.token})
       var bonplan = []
-      console.log('dede',user.idBonPlan.length )
       if (user.idBonPlan.length != 0){
         for (var i=0 ; i<user.idBonPlan.length ; i++) {
           var bonPlan00 = await bonPlanModel.findOne({_id:user.idBonPlan[i]});
-          console.log('bonplan00', bonPlan00)
           var posteur = await userModel.findOne({_id:bonPlan00.idUserPosteur});
-          bonplan.push({_id:bonPlan00._id, urlphoto:bonPlan00.urlphoto, tokenposteur:posteur.token, titre:bonPlan00.titre,  categorie:bonPlan00.categorie, description:bonPlan00.description,posteurFirstName:posteur.firstName, posteurAvatar:posteur.urlavatar
+          bonplan.push({_id:bonPlan00._id, urlphoto:bonPlan00.urlphoto, tokenposteur:posteur.token, titre:bonPlan00.titre,  categorie:bonPlan00.categorie, description:bonPlan00.description,posteurFirstName:posteur.firstName, posteurAvatar:posteur.urlAvatar
           })
         }
       }      
     } else {
       if(req.query.key == 3){
-        console.log('get favorite 1234', req.query)
         var user = await userModel.findOne({token:req.query.token})
         var myBonPlan = await bonPlanModel.find({idUserPosteur:user._id})
         var bonplan = []
-        console.log('dede',myBonPlan.length )
         if (myBonPlan.length != 0){
           for (var i=0 ; i<myBonPlan.length ; i++) {
             bonplan.push({_id:myBonPlan[i]._id, urlphoto:myBonPlan[i].urlphoto, tokenposteur:user.token, titre:myBonPlan[i].titre,  categorie:myBonPlan[i].categorie, description:myBonPlan[i].description,posteurFirstName:user.firstName, posteurAvatar:user.urlavatar
@@ -264,13 +269,11 @@ router.get('/get-bonplan', async function(req, res, next) {
       }
     }
   }
-  console.log('be50', bonplan)
   res.json({bonplan});
 });
 
 /* --- gérer les favoris --- */
 router.post('/favorite', async function(req, res, next) {
-  console.log('BE addfavorite', req.body)
 
   var user = await userModel.findOne({token:req.body.token})
   if (user.idBonPlan.indexOf(req.body.idBonPlan) == -1) {
@@ -286,7 +289,6 @@ router.post('/favorite', async function(req, res, next) {
 
 /* --- afficher les conversations --- */
 router.get('/get-conversation', async function(req, res, next) {
-  console.log('rere', req.query)
   // s'il y a un tokenInterlocuteur dans le req.query, la route comprend qu'ils s'agit d'une conversation en 1to1,
   // récupère les messages avec l'interlocuteur uniquement et les renvoie au FE
   if (req.query.tokenInterlocuteur){
@@ -295,8 +297,6 @@ router.get('/get-conversation', async function(req, res, next) {
     var messagesenv = await messageModel.find({idUserEmetteur:me._id , idUserDestinataire:interlocuteur._id})
     var messagesrecu = await messageModel.find({idUserEmetteur:interlocuteur._id , idUserDestinataire:me._id})
     var conversation = []
-
-console.log('messages 1to1', messagesenv, messagesrecu)
 
     for (var i=0 ; i<messagesenv.length ; i++){
       conversation.push({emetteur:me.token, firstName:me.firstName , contenu:messagesenv[i].contenu , date:messagesenv[i].date})
@@ -316,17 +316,12 @@ console.log('messages 1to1', messagesenv, messagesrecu)
       return 0;
     });
 
-   
-console.log('2eme messages 1to1', conversation)
-
   } else {
-
 
    // Récupération de mes infos à partir de mon token
    // Récupération des mes messages émis et envoyés
    // Création d'un tableau de contact00 qui servira à créer et vérifier une liste d'interlocuteur unique
    // Création d'un tableau de contact qui contiendra les info de mes interlocuteurs + les infos du dernier message échangé
-  console.log('get-conversation 01')
   var me = await userModel.findOne({token:req.query.token})
   var messagesenv = await messageModel.find({idUserEmetteur:me._id})
   var messagesrecu = await messageModel.find({idUserDestinataire:me._id})
@@ -355,32 +350,24 @@ console.log('2eme messages 1to1', conversation)
       contact.push({avatarInterlocuteur:interlocuteur.urlAvatar , firstNameInterlocuteur:interlocuteur.firstName , tokenInterlocuteur:interlocuteur.token , contenu:messagesrecu[i].contenu , isRead:messagesrecu[i].isRead , date:messagesrecu[i].date})
     } else {
       var lastmessage = contact.filter(message => message.tokenInterlocuteur == interlocuteur.token)
-      console.log('lastmessage', lastmessage)
-      console.log('date', messagesrecu[i].date ,lastmessage[0].date)
       if (messagesrecu[i].date > lastmessage[0].date) {
-        console.log('dd1', contact)
         contact = contact.filter(message => message.tokenInterlocuteur != interlocuteur.token)
-        console.log('dd2', contact)
         contact.push({avatarInterlocuteur:interlocuteur.urlAvatar , firstNameInterlocuteur:interlocuteur.firstName , tokenInterlocuteur:interlocuteur.token , contenu:messagesrecu[i].contenu , isRead:messagesrecu[i].isRead , date:messagesrecu[i].date})
       }
     }
   }
 }
-
-  console.log('get-conversation 01', contact)
   res.json({contact, interlocuteur, conversation});
 });
 
 
 /* --- envoyer un message en BDD --- */
 router.post('/send-message', async function(req, res, next) {
-  console.log('sendMessage', req.body)
 
   var me = await userModel.findOne({token:req.body.token})
   var interlocuteur = await userModel.findOne({token:req.body.tokenInterlocuteur})
   var date = new Date()
-  
- 
+   
   var message = new messageModel({
     idUserEmetteur:me._id,
     idUserDestinataire:interlocuteur._id,
@@ -394,22 +381,50 @@ router.post('/send-message', async function(req, res, next) {
   res.json({});
 });
 
+
+
+
 /* --- ajouter un bon plan --- */
 router.post('/add-bonplan', async function(req, res, next) {
-  console.log('addBonPlan', req.body)
 
-  var me = await userModel.findOne({token:req.body.token})
+  if (req.files) {
 
-  var bonplan = new bonPlanModel({
-    idUserPosteur: me._id,
-    titre: req.body.titre,
-    categorie: req.body.categorie,
-    description: req.body.description,
-      })
-      
-  bonplan = await bonplan.save()    
-	
-  res.json({});
+    var imagePath = './tmp/picture'+ uniqid()+'.jpg';
+    var resultCopy = await req.files.picture.mv(imagePath);
+    try {var resultCloudinary = await cloudinary.uploader.upload(imagePath)}
+    catch(error) { console.log('error', error); var erreur = error}
+    
+    if(!erreur) {
+    res.json({result: true, url:resultCloudinary.secure_url} );
+    } else {
+    res.json({result: false, message: erreur} );
+    }
+    fs.unlinkSync(imagePath);
+
+  } else {
+
+    if (req.body.url) {
+
+      var bonplan = await bonPlanModel.findOne({_id:req.body.idBonPlan});
+      bonplan.urlphoto.push(req.body.url)
+      bonplan = await bonplan.save();
+      res.json({} );
+
+    } else {
+        var me = await userModel.findOne({token:req.body.token})
+
+        var bonplan = new bonPlanModel({
+          idUserPosteur: me._id,
+          titre: req.body.titre,
+          categorie: req.body.categorie,
+          description: req.body.description,
+       })
+            
+        bonplan = await bonplan.save()    
+        
+        res.json({idBonPlan:bonplan._id});
+      }
+    }
 });
 
 /* --- supprimer un bon plan --- */
@@ -417,8 +432,7 @@ router.delete('/delete-bonplan', async function(req, res, next) {
   console.log('delete', req.body)
   var user = await userModel.find({token:req.query.token})
   var bonplan = await bonPlanModel.deleteOne({_id:req.query.id})
- 
-	
+ 	
   res.json({user});
 });
 
