b3c069e7a6e6180afaa99b7dc9b489593d9a8619
Emmanuel KONTZLER <kontzleremmanuel@yahoo.fr>
2021-01-07 21:11:29 +0100

    commit pour heroku

--- app.js
+++ app.js
@@ -24,8 +24,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'reactapp/build', 'index.html')));
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
 
+app.get('/*', (req,res)=> {
+  res.sendFile(path.join(__dirname,'/reactapp/build', 'index.html'));
+  });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
